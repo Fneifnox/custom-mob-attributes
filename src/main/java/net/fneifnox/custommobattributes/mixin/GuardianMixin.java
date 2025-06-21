10553a74c3fd97d006f75e722d7586a33cdca0b4
Fneifnox <tim.stier9@gmail.com>
2025-06-21 12:19:30 +0200

    1.0.2

--- src/main/java/net/fneifnox/custommobattributes/mixin/GuardianMixin.java
+++ src/main/java/net/fneifnox/custommobattributes/mixin/GuardianMixin.java
@@ -3,7 +3,6 @@ package net.fneifnox.custommobattributes.mixin;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.fneifnox.custommobattributes.Updater;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
-import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.GuardianEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
