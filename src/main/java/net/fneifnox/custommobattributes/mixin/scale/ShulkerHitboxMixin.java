d56c058cfb6d278f8475f293ab0c21126562feec
Fneifnox <tim.stier9@gmail.com>
2025-08-01 19:40:19 +0200

    Fixed Shulker Hitbox

--- src/main/java/net/fneifnox/custommobattributes/mixin/scale/ShulkerHitboxMixin.java
+++ src/main/java/net/fneifnox/custommobattributes/mixin/scale/ShulkerHitboxMixin.java
@@ -1,14 +1,11 @@
 package net.fneifnox.custommobattributes.mixin.scale;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.fneifnox.custommobattributes.CustomMobAttributes;
 import net.minecraft.entity.mob.ShulkerEntity;
 import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Direction;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ShulkerEntity.class)
@@ -21,17 +18,14 @@ public class ShulkerHitboxMixin {
 
         double scale = CustomMobAttributes.getScaleAttributeModifierValue(shulker, CustomMobAttributes.SCALE);
 
-        // Mittelpunkt auf X und Z
         double cx = (originalBox.minX + originalBox.maxX) / 2.0;
         double cz = (originalBox.minZ + originalBox.maxZ) / 2.0;
 
-        // Halbe Länge auf X und Z nach Skalierung
-        double dx = (originalBox.getLengthX() / 2.0) * scale;
-        double dz = (originalBox.getLengthZ() / 2.0) * scale;
+        double dx = (originalBox.getXLength() / 2.0) * scale;
+        double dz = (originalBox.getZLength() / 2.0) * scale;
 
-        // Y-Achse: minY fix, maxY skaliert
         double minY = originalBox.minY;
-        double maxY = originalBox.minY + (originalBox.getLengthY() * scale);
+        double maxY = originalBox.minY + (originalBox.getYLength() * scale);
 
         Box scaledBox = new Box(cx - dx, minY, cz - dz, cx + dx, maxY, cz + dz);
         cir.setReturnValue(scaledBox);
