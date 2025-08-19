1485bea810f57e4c7a65f098d2fbfff6073331ef
Fneifnox <tim.stier9@gmail.com>
2025-08-19 15:02:39 +0200

    2.2.1 Update

--- src/main/java/net/fneifnox/custommobattributes/mixin/movement/BatSpeedMixin.java
+++ src/main/java/net/fneifnox/custommobattributes/mixin/movement/BatSpeedMixin.java
@@ -9,34 +9,29 @@ import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.*;
 
 import static net.fneifnox.custommobattributes.CustomMobAttributes.CONFIG;
 
 @Mixin(BatEntity.class)
-public class BatSpeedMixin extends MobEntity {
+public class BatSpeedMixin {
 
     @Shadow @Nullable private BlockPos hangingPosition;
 
-    public BatSpeedMixin(EntityType<? extends MobEntity> entityType, World world) {
-        super(entityType, world);
-    }
+    @ModifyVariable(method = "mobTick", at = @At("STORE"), ordinal = 1)
+    private Vec3d modifyForwardSpeed(Vec3d original) {
+        BatEntity bat = (BatEntity)(Object)this;
 
-    @Inject(method = "mobTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/passive/BatEntity;setVelocity(Lnet/minecraft/util/math/Vec3d;)V"), cancellable = true)
-    private void modifyForwardSpeed(CallbackInfo ci) {
+        double d = (double)hangingPosition.getX() + 0.5 - bat.getX();
+        double e = (double)hangingPosition.getY() + 0.1 - bat.getY();
+        double f = (double)hangingPosition.getZ() + 0.5 - bat.getZ();
 
-        double d = (double)hangingPosition.getX() + 0.5 - this.getX();
-        double e = (double)hangingPosition.getY() + 0.1 - this.getY();
-        double f = (double)hangingPosition.getZ() + 0.5 - this.getZ();
-
-        Vec3d finalVec3d = this.getVelocity();
+        Vec3d finalVec3d = bat.getVelocity();
         Vec3d finalVec3d2 = finalVec3d.add
                 ((Math.signum(d) * 0.5 - finalVec3d.x) * 0.10000000149011612 * CONFIG.speedMultiplierForBat() * CONFIG.speedMultiplierForAll(),
                         (Math.signum(e) * 0.699999988079071 - finalVec3d.y) * 0.10000000149011612 * CONFIG.speedMultiplierForBat() * CONFIG.speedMultiplierForAll(),
                         (Math.signum(f) * 0.5 - finalVec3d.z) * 0.10000000149011612 * CONFIG.speedMultiplierForBat() * CONFIG.speedMultiplierForAll());
-        this.setVelocity(finalVec3d2);
-        ci.cancel();
+        return finalVec3d2;
+
     }
 }
