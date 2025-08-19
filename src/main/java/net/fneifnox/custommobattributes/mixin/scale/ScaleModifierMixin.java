82e7e612698952c518c82db80cbe7a064fdc2951
Fneifnox <tim.stier9@gmail.com>
2025-08-19 15:02:39 +0200

    2.2.1 Update

--- src/main/java/net/fneifnox/custommobattributes/mixin/scale/ScaleModifierMixin.java
+++ src/main/java/net/fneifnox/custommobattributes/mixin/scale/ScaleModifierMixin.java
@@ -4,12 +4,8 @@ import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.fneifnox.custommobattributes.CustomMobAttributes;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.attribute.EntityAttribute;
-import net.minecraft.entity.attribute.EntityAttributeInstance;
 import net.minecraft.entity.boss.WitherEntity;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,8 +14,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 // Priority = 500 for compat reasons
 @Mixin(value = LivingEntity.class, priority = 500)
     public abstract class ScaleModifierMixin {
-
-    @Shadow @Nullable public abstract EntityAttributeInstance getAttributeInstance(EntityAttribute attribute);
 
     @Unique
     private double lastScale = 1.0;
