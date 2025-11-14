7e6edb89f4b8e3d57ead259a60deb877e393b3d8
Fneifnox <tim.stier9@gmail.com>
2025-11-14 14:36:27 +0100

    2.4.3 Update (Translation Fix and Clean up)

--- src/main/java/net/fneifnox/custommobattributes/init/compat/BetterEndCompat.java
+++ src/main/java/net/fneifnox/custommobattributes/init/compat/BetterEndCompat.java
@@ -4,7 +4,6 @@ import net.fneifnox.custommobattributes.AttributeUpdater;
 import net.minecraft.entity.EntityType;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
-import net.minecraft.world.World;
 
 import static net.fneifnox.custommobattributes.CustomMobAttributes.CONFIG;
 
@@ -19,38 +18,32 @@ public class BetterEndCompat {
         EntityType silkMoth = Registries.ENTITY_TYPE.get(Identifier.of("betterend", "silk_moth"));
 
         AttributeUpdater.ATTRIBUTE_HANDLERS.put(cubozoa, entity -> {
-            World world = entity.getWorld();
-            AttributeUpdater.configureEntityAttributes(world, cubozoa, CONFIG.betterEnd::healthMultiplierForCubozoa, null, null, null
+            AttributeUpdater.configureEntityAttributes(entity.getWorld(), cubozoa, CONFIG.betterEnd::healthMultiplierForCubozoa, null, null, null
             );
         });
 
         AttributeUpdater.ATTRIBUTE_HANDLERS.put(dragonfly, entity -> {
-            World world = entity.getWorld();
-            AttributeUpdater.configureEntityAttributes(world, dragonfly, CONFIG.betterEnd::healthMultiplierForDragonfly, null, null, CONFIG.betterEnd::scaleMultiplierForDragonfly
+            AttributeUpdater.configureEntityAttributes(entity.getWorld(), dragonfly, CONFIG.betterEnd::healthMultiplierForDragonfly, null, null, CONFIG.betterEnd::scaleMultiplierForDragonfly
             );
         });
 
         AttributeUpdater.ATTRIBUTE_HANDLERS.put(endFish, entity -> {
-            World world = entity.getWorld();
-            AttributeUpdater.configureEntityAttributes(world, endFish, CONFIG.betterEnd::healthMultiplierForEndFish, null, null, null
+            AttributeUpdater.configureEntityAttributes(entity.getWorld(), endFish, CONFIG.betterEnd::healthMultiplierForEndFish, null, null, null
             );
         });
 
         AttributeUpdater.ATTRIBUTE_HANDLERS.put(endSlime, entity -> {
-            World world = entity.getWorld();
-            AttributeUpdater.configureEntityAttributes(world, endSlime, CONFIG.betterEnd::healthMultiplierForEndSlime, CONFIG.betterEnd::damageMultiplierForEndSlime, CONFIG.betterEnd::speedMultiplierForEndSlime, CONFIG.betterEnd::scaleMultiplierForEndSlime
+            AttributeUpdater.configureEntityAttributes(entity.getWorld(), endSlime, CONFIG.betterEnd::healthMultiplierForEndSlime, CONFIG.betterEnd::damageMultiplierForEndSlime, CONFIG.betterEnd::speedMultiplierForEndSlime, CONFIG.betterEnd::scaleMultiplierForEndSlime
             );
         });
 
         AttributeUpdater.ATTRIBUTE_HANDLERS.put(shadowWalker, entity -> {
-            World world = entity.getWorld();
-            AttributeUpdater.configureEntityAttributes(world, shadowWalker, CONFIG.betterEnd::healthMultiplierForShadowWalker, CONFIG.betterEnd::damageMultiplierForShadowWalker, CONFIG.betterEnd::speedMultiplierForShadowWalker, CONFIG.betterEnd::scaleMultiplierForShadowWalker
+            AttributeUpdater.configureEntityAttributes(entity.getWorld(), shadowWalker, CONFIG.betterEnd::healthMultiplierForShadowWalker, CONFIG.betterEnd::damageMultiplierForShadowWalker, CONFIG.betterEnd::speedMultiplierForShadowWalker, CONFIG.betterEnd::scaleMultiplierForShadowWalker
             );
         });
 
         AttributeUpdater.ATTRIBUTE_HANDLERS.put(silkMoth, entity -> {
-            World world = entity.getWorld();
-            AttributeUpdater.configureEntityAttributes(world, silkMoth, CONFIG.betterEnd::healthMultiplierForSilkMoth, null, null, CONFIG.betterEnd::scaleMultiplierForSilkMoth
+            AttributeUpdater.configureEntityAttributes(entity.getWorld(), silkMoth, CONFIG.betterEnd::healthMultiplierForSilkMoth, null, null, CONFIG.betterEnd::scaleMultiplierForSilkMoth
             );
         });
     }
--- src/main/java/net/fneifnox/custommobattributes/init/compat/VanillaBackportCompat.java
+++ src/main/java/net/fneifnox/custommobattributes/init/compat/VanillaBackportCompat.java
@@ -4,7 +4,6 @@ import net.fneifnox.custommobattributes.AttributeUpdater;
 import net.minecraft.entity.EntityType;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
-import net.minecraft.world.World;
 
 import static net.fneifnox.custommobattributes.CustomMobAttributes.CONFIG;
 
@@ -15,14 +14,12 @@ public class VanillaBackportCompat {
         EntityType happyGhast = Registries.ENTITY_TYPE.get(Identifier.of("vanillabackport", "happy_ghast"));
 
         AttributeUpdater.ATTRIBUTE_HANDLERS.put(creaking, entity -> {
-            World world = entity.getWorld();
-            AttributeUpdater.configureEntityAttributes(world, creaking, CONFIG.vanillaBackport::healthMultiplierForCreaking, CONFIG.vanillaBackport::damageMultiplierForCreaking, CONFIG.vanillaBackport::speedMultiplierForCreaking, CONFIG.vanillaBackport::scaleMultiplierForCreaking
+            AttributeUpdater.configureEntityAttributes(entity.getWorld(), creaking, CONFIG.vanillaBackport::healthMultiplierForCreaking, CONFIG.vanillaBackport::damageMultiplierForCreaking, CONFIG.vanillaBackport::speedMultiplierForCreaking, CONFIG.vanillaBackport::scaleMultiplierForCreaking
             );
         });
 
         AttributeUpdater.ATTRIBUTE_HANDLERS.put(happyGhast, entity -> {
-            World world = entity.getWorld();
-            AttributeUpdater.configureEntityAttributes(world, happyGhast, CONFIG.vanillaBackport::healthMultiplierForHappyGhast, CONFIG.vanillaBackport::damageMultiplierForHappyGhast, CONFIG.vanillaBackport::speedMultiplierForHappyGhast, null
+            AttributeUpdater.configureEntityAttributes(entity.getWorld(), happyGhast, CONFIG.vanillaBackport::healthMultiplierForHappyGhast, CONFIG.vanillaBackport::damageMultiplierForHappyGhast, CONFIG.vanillaBackport::speedMultiplierForHappyGhast, null
             );
         });
     }
