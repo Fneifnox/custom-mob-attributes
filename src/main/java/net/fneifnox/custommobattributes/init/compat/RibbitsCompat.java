bbebdcc16043272308cefe845780f6ec6a3f85ec
Fneifnox <tim.stier9@gmail.com>
2025-12-06 15:34:13 +0100

    2.5.0 Update

---
+++ src/main/java/net/fneifnox/custommobattributes/init/compat/RibbitsCompat.java
@@ -0,0 +1,20 @@
+package net.fneifnox.custommobattributes.init.compat;
+
+import net.fneifnox.custommobattributes.AttributeUpdater;
+import net.minecraft.entity.EntityType;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
+
+import static net.fneifnox.custommobattributes.CustomMobAttributes.CONFIG;
+
+public class RibbitsCompat {
+
+    public static void initRibbitsAttributeHandlers() {
+        EntityType ribbit = Registries.ENTITY_TYPE.get(Identifier.of("ribbits", "ribbit"));
+
+        AttributeUpdater.ATTRIBUTE_HANDLERS.put(ribbit, entity -> {
+            AttributeUpdater.configureEntityAttributes(entity.getWorld(), ribbit, CONFIG.ribbits::healthMultiplierForRibbit, CONFIG.ribbits::damageMultiplierForRibbit, CONFIG.ribbits::speedMultiplierForRibbit, CONFIG.ribbits::scaleMultiplierForRibbit
+            );
+        });
+    }
+}
