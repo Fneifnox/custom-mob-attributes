e2c3de47b542c8503c6e1503df2a047b16c94e63
Fneifnox <tim.stier9@gmail.com>
2025-12-27 18:24:45 +0100

    3.0.4 Update

--- src/main/java/net/fneifnox/custommobattributes/mixin/scale/EntityRidingScaleMixin.java
+++ src/main/java/net/fneifnox/custommobattributes/mixin/scale/EntityRidingScaleMixin.java
@@ -14,18 +14,20 @@ public abstract class EntityRidingScaleMixin {
     @Inject(method = "updatePassengerPosition(Lnet/minecraft/entity/Entity;Lnet/minecraft/entity/Entity$PositionUpdater;)V",
             at = @At("HEAD"), cancellable = true)
     private void onUpdatePassengerPosition(Entity passenger, Entity.PositionUpdater positionUpdater, CallbackInfo ci) {
-        LivingEntity livingEntity = (LivingEntity)(Object)this;
-        if (!((Object)(this) instanceof LivingEntity)) return;
-        if (!livingEntity.hasPassenger(passenger)) return;
+        Entity entity = (Entity)(Object)this;
 
-        double scale = livingEntity.getAttributeValue(CustomMobAttributes.SCALE);
+        if (!entity.hasPassenger(passenger)) return;
 
-        double posX = livingEntity.getX();
-        double posY = livingEntity.getY() + (livingEntity.getHeight() - (livingEntity.getHeight() / scale / 1.85));
-        double posZ = livingEntity.getZ();
+        if (entity instanceof LivingEntity livingEntity) {
+            double scale = livingEntity.getAttributeValue(CustomMobAttributes.SCALE);
 
-        positionUpdater.accept(passenger, posX, posY, posZ);
+            double posX = livingEntity.getX();
+            double posY = livingEntity.getY() + (livingEntity.getHeight() - (livingEntity.getHeight() / scale / 1.85));
+            double posZ = livingEntity.getZ();
 
-        ci.cancel();
+            positionUpdater.accept(passenger, posX, posY, posZ);
+
+            ci.cancel();
+        }
     }
 }
