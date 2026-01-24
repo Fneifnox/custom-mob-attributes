898948e6fd2fdc5280f3e0e08fd7178ccaf0af45
Fneifnox <tim.stier9@gmail.com>
2026-01-24 15:08:00 +0100

    3.0.6 Update

--- src/main/java/net/fneifnox/custommobattributes/mixin/damage/ArrowMixin.java
+++ src/main/java/net/fneifnox/custommobattributes/mixin/damage/ArrowMixin.java
@@ -1,5 +1,7 @@
 package net.fneifnox.custommobattributes.mixin.damage;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
@@ -14,7 +16,6 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.Objects;
 
@@ -22,8 +23,8 @@ import static net.fneifnox.custommobattributes.CustomMobAttributes.CONFIG;
 
 @Mixin(PersistentProjectileEntity.class)
 public class ArrowMixin {
-    @Redirect(method = "onEntityHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;sidedDamage(Lnet/minecraft/entity/damage/DamageSource;F)Z"))
-    private boolean redirectDamage(Entity entity, DamageSource source, float originalDamage) {
+    @WrapOperation(method = "onEntityHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;sidedDamage(Lnet/minecraft/entity/damage/DamageSource;F)Z"))
+    private boolean redirectDamage(Entity entity, DamageSource source, float originalDamage, Operation<Boolean> original) {
         PersistentProjectileEntity projectile = (PersistentProjectileEntity)(Object)this;
         if (projectile instanceof SpectralArrowEntity && projectile.getOwner() instanceof LivingEntity || projectile instanceof ArrowEntity && projectile.getOwner() instanceof LivingEntity) {
             double multiplier = 1f;
--- src/main/java/net/fneifnox/custommobattributes/mixin/damage/ShulkerBulletMixin.java
+++ src/main/java/net/fneifnox/custommobattributes/mixin/damage/ShulkerBulletMixin.java
@@ -1,17 +1,24 @@
 package net.fneifnox.custommobattributes.mixin.damage;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.projectile.ShulkerBulletEntity;
+import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import static net.fneifnox.custommobattributes.CustomMobAttributes.CONFIG;
 
 @Mixin(ShulkerBulletEntity.class)
 public class ShulkerBulletMixin {
-    @ModifyArg(method = "onEntityHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;sidedDamage(Lnet/minecraft/entity/damage/DamageSource;F)Z"))
-    private float injectCustomDamage(float originalDamage) {
+    @WrapOperation(method = "onEntityHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;sidedDamage(Lnet/minecraft/entity/damage/DamageSource;F)Z"))
+    private boolean injectCustomDamage(Entity entity, DamageSource source, float originalDamage, Operation<Boolean> original) {
         double multiplier = CONFIG.damageMultiplierForShulker() * CONFIG.damageMultiplierForAll();
-        return (float) (originalDamage * multiplier);
+        if (entity.getWorld() instanceof ServerWorld serverWorld) {
+            return entity.damage(serverWorld, source, (float) (originalDamage * multiplier));
+        }
+        return false;
     }
 }
