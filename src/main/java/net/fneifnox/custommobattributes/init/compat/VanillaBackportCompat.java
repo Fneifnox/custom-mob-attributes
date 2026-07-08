34da0fa7e6a88580e3bee21c7ef5dae73c01eb26
Fneifnox <tim.stier9@gmail.com>
2026-07-08 18:24:21 +0200

    3.0.8 Update

--- src/main/java/net/fneifnox/custommobattributes/init/compat/VanillaBackportCompat.java
+++ src/main/java/net/fneifnox/custommobattributes/init/compat/VanillaBackportCompat.java
@@ -5,21 +5,35 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 
+import static net.fneifnox.custommobattributes.AttributeUpdater.ATTRIBUTE_HANDLERS;
+import static net.fneifnox.custommobattributes.AttributeUpdater.configureEntityAttributes;
 import static net.fneifnox.custommobattributes.CustomMobAttributes.CONFIG;
 
 public class VanillaBackportCompat {
 
     public static void initVanillaBackportAttributeHandlers() {
-        EntityType creaking = Registries.ENTITY_TYPE.get(Identifier.of("vanillabackport", "creaking"));
-        EntityType happyGhast = Registries.ENTITY_TYPE.get(Identifier.of("vanillabackport", "happy_ghast"));
+        EntityType armadillo = Registries.ENTITY_TYPE.get(Identifier.of("minecraft", "armadillo"));
+        EntityType creaking = Registries.ENTITY_TYPE.get(Identifier.of("minecraft", "creaking"));
+        EntityType happyGhast = Registries.ENTITY_TYPE.get(Identifier.of("minecraft", "happy_ghast"));
+
+        ATTRIBUTE_HANDLERS.put(armadillo, entity -> {
+            if (entity.isBaby()) {
+                configureEntityAttributes(entity.getWorld(), entity, CONFIG.vanillaBackport.babyArmadillo::healthMultiplierForBabyArmadillo, null, CONFIG.vanillaBackport.babyArmadillo::healthMultiplierForBabyArmadillo, CONFIG.vanillaBackport.babyArmadillo::healthMultiplierForBabyArmadillo, CONFIG.vanillaBackport::healthMultiplierForArmadillo, null, CONFIG.vanillaBackport::speedMultiplierForArmadillo, CONFIG.vanillaBackport::scaleMultiplierForArmadillo
+                );
+            }
+            else {
+                configureEntityAttributes(entity.getWorld(), entity, CONFIG.vanillaBackport::healthMultiplierForArmadillo, null, CONFIG.vanillaBackport::speedMultiplierForArmadillo, CONFIG.vanillaBackport::scaleMultiplierForArmadillo
+                );
+            }
+        });
 
         AttributeUpdater.ATTRIBUTE_HANDLERS.put(creaking, entity -> {
             AttributeUpdater.configureEntityAttributes(entity.getWorld(), entity, CONFIG.vanillaBackport::healthMultiplierForCreaking, CONFIG.vanillaBackport::damageMultiplierForCreaking, CONFIG.vanillaBackport::speedMultiplierForCreaking, CONFIG.vanillaBackport::scaleMultiplierForCreaking
             );
         });
 
-        AttributeUpdater.ATTRIBUTE_HANDLERS.put(happyGhast, entity -> {
-            AttributeUpdater.configureEntityAttributes(entity.getWorld(), entity, CONFIG.vanillaBackport::healthMultiplierForHappyGhast, CONFIG.vanillaBackport::damageMultiplierForHappyGhast, CONFIG.vanillaBackport::speedMultiplierForHappyGhast, null
+        ATTRIBUTE_HANDLERS.put(happyGhast, entity -> {
+            AttributeUpdater.configureEntityAttributes(entity.getWorld(), entity, CONFIG.vanillaBackport::healthMultiplierForHappyGhast, null, CONFIG.vanillaBackport::speedMultiplierForHappyGhast, null
             );
         });
     }
